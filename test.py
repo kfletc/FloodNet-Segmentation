4cb9aa9db4aac815311f6c1277f63004614bdc36
Kyle Fletcher <kf0040@uah.edu>
2024-04-30 23:47:55 -0500

    renamed predict to test, removed plotting from test, reformatted some files

---
+++ test.py
@@ -0,0 +1,88 @@
+# import the nexessary packages
+from torch.nn import CrossEntropyLoss
+
+import config
+from dataset import FloodDataset
+import numpy as np
+import torch
+import os
+from imutils import paths
+from tqdm import tqdm
+import pickle
+from torch.utils.data import DataLoader
+from torchvision import transforms
+
+if os.path.exists('PickleDumps/test_pickle'):
+    pickle_file = open('PickleDumps/test_pickle', 'rb')
+    testDS = pickle.load(pickle_file)
+    pickle_file.close()
+else:
+    test_imagePaths = sorted(list(paths.list_images(config.IMAGE_TEST_DATASET_PATH)))
+    test_maskPaths = sorted(list(paths.list_images(config.MASK_TEST_DATASET_PATH)))
+
+    # define transformations
+    transforms_image = transforms.Compose([transforms.ToPILImage(),
+                                           transforms.Resize((config.INPUT_IMAGE_HEIGHT, config.INPUT_IMAGE_WIDTH)),
+                                           transforms.ToTensor()])
+
+    transforms_mask = transforms.Compose([transforms.ToPILImage(),
+                                          transforms.Resize((config.INPUT_IMAGE_HEIGHT, config.INPUT_IMAGE_WIDTH)),
+                                          transforms.PILToTensor()])
+
+    testDS = FloodDataset(imagePaths=test_imagePaths, maskPaths=test_maskPaths,
+                          transforms=transforms_image, transforms_mask=transforms_mask)
+
+print(f"[INFO] found {len(testDS)} examples in the test set...")
+
+testLoader = DataLoader(testDS, shuffle=False,
+                        batch_size=config.BATCH_SIZE, pin_memory=config.PIN_MEMORY,
+                        num_workers=0)
+
+print("[INFO] load up model...")
+unet = torch.load(config.MODEL_PATH).to(config.DEVICE)
+
+# results from test data
+classes = ['Background', 'Building Flooded', 'Building Non-Flooded', 'Road Flooded', 'Road Non-Flooded',
+           'Water', 'Tree', 'Vehicle', 'Pool', 'Grass']
+
+testSteps = len(testDS) // config.BATCH_SIZE
+lossFunc = CrossEntropyLoss()
+totalTestLoss = 0
+totalTestAcc = 0
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+
+for x, y in tqdm(testLoader):
+    # send the input to the device
+    (x, y) = (x.to(config.DEVICE), y.to(config.DEVICE))
+
+    pred = unet(x)
+    y_squeeze = torch.squeeze(y)
+    loss = lossFunc(pred, y_squeeze)
+    totalTestLoss += loss
+    # calculate pixel accuracy
+    _, class_preds = torch.max(pred, 1)
+    y_preds = y_squeeze.data.view_as(class_preds)
+    y_preds_cpu = y_preds.cpu()
+    correct_tensor = class_preds.eq(y_preds)
+    correct = correct_tensor.cpu().numpy()
+    total_correct = np.sum(correct)
+    totalTestAcc += total_correct / (config.INPUT_IMAGE_WIDTH * config.INPUT_IMAGE_HEIGHT * config.BATCH_SIZE) * 100.0
+    for b in range(config.BATCH_SIZE):
+        for c, x_data in enumerate(y_preds_cpu[b]):
+            for d, xy_data in enumerate(x_data):
+                label = xy_data.data
+                class_correct[label] += correct[b][c][d].item()
+                class_total[label] += 1
+
+avgTestLoss = totalTestLoss / testSteps
+avgTestAcc = totalTestAcc / testSteps
+print("Test loss: {:.6f}".format(avgTestLoss))
+print("Test acc: {:.6f}".format(avgTestAcc))
+
+for i in range(10):
+    if class_total[i] > 0:
+        print('Test Accuracy of %5s: %2d%%' % (
+            classes[i], 100 * class_correct[i] / class_total[i]))
+    else:
+        print('Test Accuracy of %5s: N/A (no training examples)' % (classes[i]))
--- train.py
+++ train.py
@@ -26,61 +26,60 @@ val_maskPaths = sorted(list(paths.list_images(config.MASK_VAL_DATASET_PATH)))
 
 # define transformations
 transforms_image = transforms.Compose([transforms.ToPILImage(),
-			       transforms.Resize((config.INPUT_IMAGE_HEIGHT,
-						  config.INPUT_IMAGE_WIDTH)),
-			       transforms.ToTensor()])
+                                       transforms.Resize((config.INPUT_IMAGE_HEIGHT,
+                                                          config.INPUT_IMAGE_WIDTH)),
+                                       transforms.ToTensor()])
 
 transforms_mask = transforms.Compose([transforms.ToPILImage(),
-			       transforms.Resize((config.INPUT_IMAGE_HEIGHT,
-						  config.INPUT_IMAGE_WIDTH)),
-			       transforms.PILToTensor()])
+                                      transforms.Resize((config.INPUT_IMAGE_HEIGHT,
+                                                         config.INPUT_IMAGE_WIDTH)),
+                                      transforms.PILToTensor()])
 
 # create the datasets
 if os.path.exists('PickleDumps/train_pickle'):
-	pickle_file = open('PickleDumps/train_pickle', 'rb')
-	trainDS = pickle.load(pickle_file)
-	pickle_file.close()
+    pickle_file = open('PickleDumps/train_pickle', 'rb')
+    trainDS = pickle.load(pickle_file)
+    pickle_file.close()
 else:
-	trainDS = FloodDataset(imagePaths=train_imagePaths, maskPaths=train_maskPaths,
-				   		transforms=transforms_image, transforms_mask=transforms_mask)
-	with open('PickleDumps/train_pickle', 'wb') as train_file:
-		pickle.dump(trainDS, train_file)
+    trainDS = FloodDataset(imagePaths=train_imagePaths, maskPaths=train_maskPaths,
+                           transforms=transforms_image, transforms_mask=transforms_mask)
+    with open('PickleDumps/train_pickle', 'wb') as train_file:
+        pickle.dump(trainDS, train_file)
 
 if os.path.exists('PickleDumps/test_pickle'):
-	pickle_file = open('PickleDumps/test_pickle', 'rb')
-	testDS = pickle.load(pickle_file)
-	pickle_file.close()
+    pickle_file = open('PickleDumps/test_pickle', 'rb')
+    testDS = pickle.load(pickle_file)
+    pickle_file.close()
 else:
-	testDS = FloodDataset(imagePaths=test_imagePaths, maskPaths=test_maskPaths,
-		      			transforms=transforms_image, transforms_mask=transforms_mask)
-	with open('PickleDumps/test_pickle', 'wb') as test_file:
-		pickle.dump(testDS, test_file)
+    testDS = FloodDataset(imagePaths=test_imagePaths, maskPaths=test_maskPaths,
+                          transforms=transforms_image, transforms_mask=transforms_mask)
+    with open('PickleDumps/test_pickle', 'wb') as test_file:
+        pickle.dump(testDS, test_file)
 
 if os.path.exists('PickleDumps/val_pickle'):
-	pickle_file = open('PickleDumps/val_pickle', 'rb')
-	valDS = pickle.load(pickle_file)
-	pickle_file.close()
+    pickle_file = open('PickleDumps/val_pickle', 'rb')
+    valDS = pickle.load(pickle_file)
+    pickle_file.close()
 else:
-	valDS = FloodDataset(imagePaths=val_imagePaths, maskPaths=val_maskPaths,
-		     			transforms=transforms_image, transforms_mask=transforms_mask)
-	with open('PickleDumps/val_pickle', 'wb') as val_file:
-		pickle.dump(valDS, val_file)
+    valDS = FloodDataset(imagePaths=val_imagePaths, maskPaths=val_maskPaths,
+                         transforms=transforms_image, transforms_mask=transforms_mask)
+    with open('PickleDumps/val_pickle', 'wb') as val_file:
+        pickle.dump(valDS, val_file)
 
 print(f"[INFO] found {len(trainDS)} examples in the training set...")
 print(f"[INFO] found {len(testDS)} examples in the test set...")
 print(f"[INFO] found {len(valDS)} examples in the val set...")
 
-
 # create the training, test, and val data loaders
 trainLoader = DataLoader(trainDS, shuffle=True,
-			 batch_size=config.BATCH_SIZE, pin_memory=config.PIN_MEMORY,
-			 num_workers=0)
+                         batch_size=config.BATCH_SIZE, pin_memory=config.PIN_MEMORY,
+                         num_workers=0)
 testLoader = DataLoader(testDS, shuffle=False,
-			 batch_size=config.BATCH_SIZE, pin_memory=config.PIN_MEMORY,
-			 num_workers=0)
+                        batch_size=config.BATCH_SIZE, pin_memory=config.PIN_MEMORY,
+                        num_workers=0)
 valLoader = DataLoader(valDS, shuffle=False,
-			 batch_size=config.BATCH_SIZE, pin_memory=config.PIN_MEMORY,
-			 num_workers=0)
+                       batch_size=config.BATCH_SIZE, pin_memory=config.PIN_MEMORY,
+                       num_workers=0)
 
 # initialize UNet model
 unet = FloodNet().to(config.DEVICE)
@@ -98,7 +97,6 @@ valSteps = len(valDS) // config.BATCH_SIZE
 lowestValLoss = np.Inf
 failed_epochs = 0
 
-
 # initialize a dictionary to store training history
 H = {"train_loss": [], "val_loss": [], "train_acc": [], "val_acc": []}
 
@@ -106,96 +104,98 @@ H = {"train_loss": [], "val_loss": [], "train_acc": [], "val_acc": []}
 print("[INFO] training the network...")
 startTime = time.time()
 for e in tqdm(range(config.MAX_EPOCHS)):
-	print(f"[INFO] starting epochs {e}")
+    print(f"[INFO] starting epochs {e}")
 
-	# set the model in training mode
-	unet.train()
+    # set the model in training mode
+    unet.train()
 
-	# initialize the total training and validation loss
-	totalTrainLoss = 0
-	totalValLoss = 0
-	totalTrainAccuracy = 0
-	totalValAccuracy = 0
-	
-	# loop over the training set
-	for (x, y) in trainLoader:
-		# send the input to the device
-		(x, y) = (x.to(config.DEVICE), y.to(config.DEVICE))
+    # initialize the total training and validation loss
+    totalTrainLoss = 0
+    totalValLoss = 0
+    totalTrainAccuracy = 0
+    totalValAccuracy = 0
 
-		# preform a forward pass and calculate the training loss
-		pred = unet(x)
-		y_squeeze = torch.squeeze(y)
-		loss = lossFunc(pred, y_squeeze)
-		# calculate training pixel accuracy
-		_, class_preds = torch.max(pred, 1)
-		correct_tensor = class_preds.eq(y_squeeze.data.view_as(class_preds))
-		total_correct = np.sum(correct_tensor.cpu().numpy())
-		totalTrainAccuracy += total_correct / (config.INPUT_IMAGE_WIDTH * config.INPUT_IMAGE_HEIGHT * config.BATCH_SIZE) * 100.0
+    # loop over the training set
+    for (x, y) in trainLoader:
+        # send the input to the device
+        (x, y) = (x.to(config.DEVICE), y.to(config.DEVICE))
 
-		# first, zero out any previously accumulated gradients, then
-		# preform backproagation, and then update model parameters
-		opt.zero_grad()
-		loss.backward()
-		opt.step()
+        # preform a forward pass and calculate the training loss
+        pred = unet(x)
+        y_squeeze = torch.squeeze(y)
+        loss = lossFunc(pred, y_squeeze)
+        # calculate training pixel accuracy
+        _, class_preds = torch.max(pred, 1)
+        correct_tensor = class_preds.eq(y_squeeze.data.view_as(class_preds))
+        total_correct = np.sum(correct_tensor.cpu().numpy())
+        totalTrainAccuracy += total_correct / (
+                    config.INPUT_IMAGE_WIDTH * config.INPUT_IMAGE_HEIGHT * config.BATCH_SIZE) * 100.0
 
-		# add teh loss to the total training loss so far
-		totalTrainLoss += loss
+        # first, zero out any previously accumulated gradients, then
+        # preform backproagation, and then update model parameters
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
 
-	# switch off autograd
-	with torch.no_grad():
-		# set model in evaluation mode
-		unet.eval()
+        # add teh loss to the total training loss so far
+        totalTrainLoss += loss
 
-		# loop over the validation set
-		for (x, y) in valLoader:
-			# send the input to the device
-			(x, y) = (x.to(config.DEVICE), y.to(config.DEVICE))
+    # switch off autograd
+    with torch.no_grad():
+        # set model in evaluation mode
+        unet.eval()
 
-			# make the predictions and calculate the validation loss
-			pred = unet(x)
-			y_squeeze = torch.squeeze(y)
-			totalValLoss += lossFunc(pred, y_squeeze)
+        # loop over the validation set
+        for (x, y) in valLoader:
+            # send the input to the device
+            (x, y) = (x.to(config.DEVICE), y.to(config.DEVICE))
 
-			_, class_preds = torch.max(pred, 1)
-			correct_tensor = class_preds.eq(y_squeeze.data.view_as(class_preds))
-			total_correct = np.sum(correct_tensor.cpu().numpy())
-			totalValAccuracy += total_correct / (config.INPUT_IMAGE_WIDTH * config.INPUT_IMAGE_HEIGHT * config.BATCH_SIZE) * 100.0
+            # make the predictions and calculate the validation loss
+            pred = unet(x)
+            y_squeeze = torch.squeeze(y)
+            totalValLoss += lossFunc(pred, y_squeeze)
 
-	# calculate teh average training and validation loss
-	avgTrainLoss = totalTrainLoss / trainSteps
-	avgValLoss = totalValLoss / valSteps
-	avgTrainAccuracy = totalTrainAccuracy / trainSteps
-	avgValAccuracy = totalValAccuracy / valSteps
+            _, class_preds = torch.max(pred, 1)
+            correct_tensor = class_preds.eq(y_squeeze.data.view_as(class_preds))
+            total_correct = np.sum(correct_tensor.cpu().numpy())
+            totalValAccuracy += total_correct / (
+                        config.INPUT_IMAGE_WIDTH * config.INPUT_IMAGE_HEIGHT * config.BATCH_SIZE) * 100.0
 
-	# update our training history
-	H["train_loss"].append(avgTrainLoss.cpu().detach().numpy())
-	H["val_loss"].append(avgValLoss.cpu().detach().numpy())
-	H["train_acc"].append(avgTrainAccuracy)
-	H["val_acc"].append(avgValAccuracy)
+    # calculate teh average training and validation loss
+    avgTrainLoss = totalTrainLoss / trainSteps
+    avgValLoss = totalValLoss / valSteps
+    avgTrainAccuracy = totalTrainAccuracy / trainSteps
+    avgValAccuracy = totalValAccuracy / valSteps
 
-	# print the model training and calidation information
-	print("[INFO] EPOCH: {}/{}".format(e + 1, config.MAX_EPOCHS))
-	print("Train loss: {:.6f}, Val loss: {:.4f}".format(
-		avgTrainLoss, avgValLoss))
-	print("Train acc: {:.6f}, Val acc: {:.4f}".format(
-		avgTrainAccuracy, avgValAccuracy))
+    # update our training history
+    H["train_loss"].append(avgTrainLoss.cpu().detach().numpy())
+    H["val_loss"].append(avgValLoss.cpu().detach().numpy())
+    H["train_acc"].append(avgTrainAccuracy)
+    H["val_acc"].append(avgValAccuracy)
 
-	if avgValLoss <= lowestValLoss:
-		lowestValLoss = avgValLoss
-		failed_epochs = 0
-		torch.save(unet, config.MODEL_PATH)
-	else:
-		failed_epochs += 1
-		print("Validation Loss increased from lass epoch.\t# of failed epochs: {}".format(failed_epochs))
+    # print the model training and calidation information
+    print("[INFO] EPOCH: {}/{}".format(e + 1, config.MAX_EPOCHS))
+    print("Train loss: {:.6f}, Val loss: {:.4f}".format(
+        avgTrainLoss, avgValLoss))
+    print("Train acc: {:.6f}, Val acc: {:.4f}".format(
+        avgTrainAccuracy, avgValAccuracy))
 
-	if failed_epochs >= config.FAILED_BATCH_NUM:
-		print("Validation Loss increased for {} epochs. \nEnding training and saving model.".format(failed_epochs))
-		break
+    if avgValLoss <= lowestValLoss:
+        lowestValLoss = avgValLoss
+        failed_epochs = 0
+        torch.save(unet, config.MODEL_PATH)
+    else:
+        failed_epochs += 1
+        print("Validation Loss increased from lass epoch.\t# of failed epochs: {}".format(failed_epochs))
+
+    if failed_epochs >= config.FAILED_BATCH_NUM:
+        print("Validation Loss increased for {} epochs. \nEnding training and saving model.".format(failed_epochs))
+        break
 
 # display the total time needed to preform the training
 endTime = time.time()
 print("[INFO] total time taken to train the model: {:.2f}s".format(
-	endTime-startTime))
+    endTime - startTime))
 
 plt.style.use("ggplot")
 plt.figure()
